7627307a7fd40266c6757bea3f78da64e97057a9
BenjieA <60783997+BenjieA@users.noreply.github.com>
2023-07-08 20:27:40 +0100

    Create string.sql

---
+++ sql/data_types/string/string.sql
@@ -0,0 +1,5 @@
+create database cm_devices;
+use cm_devices;
+create table customers(username char(9), full_name varchar(100), email varchar(255));
+show columns from customers;
+create table feedback_table(feedback_ID char(8), type varchar(100), comment varchar(500));
